27ba0165f28c34dd7cbb9b80eb941aa39cab3a12
Sudarshan <sudarshanbhumireddy015@gmail.com>
2024-08-24 12:58:18 +0530

    Modified manifest files

---
+++ manifests/node.pp
@@ -0,0 +1,3 @@
+node 'nodepuppetdevops.com' {
+    Notify { 'this is sample puppet repository for testing purpose and node is nodepuppetdevops.com': }
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,2 @@
+Notify { 'this is sample puppet repository for testing purpose and from site.pp': }
 
-node default {
-  # This is where you can declare classes for all nodes
-    Notify { 'this is sample puppet repository for testing purpose': }
-}
